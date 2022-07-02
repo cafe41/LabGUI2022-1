11dcef456fa4a695cea97f89f7ab406d75372f8f
Gustavo Vergara <gustavo.vergara.p@usach.cl>
2022-07-01 20:24:40 -0400

    Tercer commit
    creé el menú jugar y le agregué botones, sin embargo estos botones no hacen nada aún y falta agregarles cosas para que funcionen.

---
+++ src/main/java/GUI/MenuJugar.java
@@ -0,0 +1,14 @@
+package main.java.GUI;
+
+import javax.swing.*;
+
+public class MenuJugar {
+    private JPanel panelJugar;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JLabel texto1;
+}
