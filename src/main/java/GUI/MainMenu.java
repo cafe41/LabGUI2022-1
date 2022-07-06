c64376984858f1ca9badca93e43dd6f3c4b80567
Gustavo Vergara <gustavo.vergara.p@usach.cl>
2022-07-06 05:19:33 -0400

    Sexto commit
    Avance interfaz, se agregaron más cosas, ahora es posible instanciar un cardsSet

--- src/main/java/GUI/MainMenu.java
+++ src/main/java/GUI/MainMenu.java
@@ -1,7 +1,8 @@
 package main.java.GUI;
 
+import main.java.Dobble.game.DobbleGame;
+
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,13 +15,14 @@ public class MainMenu extends JFrame {
     private JLabel fondoDerecho;
     private JLabel fondoIzquierdo;
 
+    public static DobbleGame game;
+
+
     /**
      * MainMenu: Constructor del menú principal,
      * La función llama al constructor de JFrame a través de "super(title)"
      * <p>
      * @param title Título de la ventana a crear.
-     * <p>
-     * @return MainMenu
      * @see MenuJugar Menú Jugar
      * @see MenuPlayer Menú para añadir/editar un jugador
      * @see MenuCards Menú para editar el mazo
@@ -32,18 +34,23 @@ public class MainMenu extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(panelPrincipal);
         this.pack();
+
+        game = new DobbleGame();
+
         jugarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFrame frame1 = new MenuJugar("Menú Jugar");
                 frame1.setVisible(true);
+                dispose();
             }
         });
         crearMazoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame frame2 = new MenuCards("Menú Cartas");
+                JFrame frame2 = new MenuCards("Menú Cartas", game);
                 frame2.setVisible(true);
+                dispose();
             }
         });
         agregarJugadoresButton.addActionListener(new ActionListener() {
@@ -51,6 +58,7 @@ public class MainMenu extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 JFrame frame3 = new MenuPlayer("Menú Jugadores");
                 frame3.setVisible(true);
+                dispose();
             }
         });
         estadoActualButton.addActionListener(new ActionListener() {
@@ -58,15 +66,66 @@ public class MainMenu extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 JFrame frame4 = new MenuStatus("Menú Estado");
                 frame4.setVisible(true);
+                dispose();
             }
         });
     }
+    /**
+     * MainMenu: Constructor del menú principal,
+     * La función llama al constructor de JFrame a través de "super(title)"
+     * <p>
+     * @param title Título de la ventana a crear.
+     * @param game DobbleGame que se "pasa" entre menus y botones.
+     * @see MenuJugar Menú Jugar
+     * @see MenuPlayer Menú para añadir/editar un jugador
+     * @see MenuCards Menú para editar el mazo
+     * @see MenuStatus Menú del estado del juego
+     */
+    public MainMenu(String title, DobbleGame game){
+        super(title);
 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panelPrincipal);
+        this.pack();
+
+        MainMenu.game = game;
+
+        jugarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame1 = new MenuJugar("Menú Jugar");
+                frame1.setVisible(true);
+                dispose();
+            }
+        });
+        crearMazoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame2 = new MenuCards("Menú Cartas", game);
+                frame2.setVisible(true);
+                dispose();
+            }
+        });
+        agregarJugadoresButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame3 = new MenuPlayer("Menú Jugadores");
+                frame3.setVisible(true);
+                dispose();
+            }
+        });
+        estadoActualButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame4 = new MenuStatus("Menú Estado");
+                frame4.setVisible(true);
+                dispose();
+            }
+        });
+    }
     /**Método main donde llamaremos las interfaces gráficas definidas previamente.
      * <p>
      * @param args
-     * <p>
-     * @return void
      */
     public static void main(String[] args) {
         JFrame frame = new MainMenu("Menú Principal");
@@ -76,15 +135,6 @@ public class MainMenu extends JFrame {
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
-        JLabel separador1 = new JLabel();
-        separador1.setText("AYUDA");
-        separador1.setIcon(new ImageIcon("GUI/JPG/Separador1.jpg"));
-        separador1.setHorizontalTextPosition(JLabel.CENTER);
-        separador1.setVerticalTextPosition(JLabel.TOP);
-        separador1.setForeground(new Color(0xBB0007));
-        separador1.setBackground(Color.BLACK);
-        separador1.setOpaque(true);
-        separador1.setBorder(BorderFactory.createLineBorder(Color.red,3));
     }
 }
 
--- src/main/java/GUI/MenuCards.java
+++ src/main/java/GUI/MenuCards.java
@@ -1,15 +1,23 @@
 package main.java.GUI;
 
+import main.java.Dobble.cardsSet.Dobble;
+import main.java.Dobble.game.DobbleGame;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuCards extends JFrame{
-
-
     private JPanel panelCards;
     private JLabel texto1;
-    private JTable mazoActualTable;
-    private JButton estadoDelMazoButton;
+    private JButton crearMazoButton;
     private JButton back;
+    private JLabel mazoActual;
+    private JButton agregarCartasButton;
+    private JButton verifyButton;
+    private JButton missingCardsButton;
+
+    public static DobbleGame game;
 
     /**
      * MenuCards: Constructor del menú que muestra el estado del juego por pantalla,
@@ -20,12 +28,37 @@ public class MenuCards extends JFrame{
      * @return MenuCards
      * @see MainMenu Menú Principal
      */
-    public MenuCards(String title) {
+    public MenuCards(String title, DobbleGame game) {
         super(title);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(panelCards);
         this.pack();
+
+        this.game = game;
+        if(game.getMazoCartas() != null){
+            mazoActual.setText("Mazo Actual de cartas: " +
+                    game.getMazoCartas().getCardsSet().toString());
+        }
+        else{
+            mazoActual.setText("Mazo Actual de cartas: null");
+        }
+
+        crearMazoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame1 = new CrearCartas("Crear Mazo", game);
+                frame1.setVisible(true);
+            }
+        });
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new MainMenu("Menú Principal", game);
+                frame.setVisible(true);
+                dispose();
+            }
+        });
     }
 
 }
